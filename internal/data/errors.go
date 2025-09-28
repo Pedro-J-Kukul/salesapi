db92817e36bc19a06da52b7adcb2dfd93b710b2e
Pedro-J-Kukul <pedro.j.kukul@gmail.com>
2025-09-28 02:36:39 -0600

    Added sales error

--- internal/data/errors.go
+++ internal/data/errors.go
@@ -5,8 +5,9 @@ import "errors"
 
 // Define custom error variables for common error scenarios.
 var (
-	ErrRecordNotFound = errors.New("record not found")
-	ErrEditConflict   = errors.New("edit conflict")
-	ErrInvalidID      = errors.New("invalid ID")
-	ErrNoRecords      = errors.New("no matching records found")
+	ErrRecordNotFound   = errors.New("record not found")
+	ErrEditConflict     = errors.New("edit conflict")
+	ErrInvalidID        = errors.New("invalid ID")
+	ErrNoRecords        = errors.New("no matching records found")
+	ErrInsufficientCash = errors.New("insufficient cash provided")
 )
